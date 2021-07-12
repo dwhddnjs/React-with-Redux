485f2f14661df96c87ab557f31bf6b680b728002
dwhddnjs <per1215@naver.com>
2021-07-13 01:15:25 +0900

    ducks patten with redux module

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./exercise";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
