57fed3f439be95e52655c03a05abee0c5c93c5e6
dwhddnjs <per1215@naver.com>
2021-07-13 23:18:31 +0900

    never used exercise.js

--- src/exercise.js
+++ src/exercise.js
@@ -1,68 +1,73 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const initialState = {
-  counter: 0,
-  text: "",
-  list: [],
-};
+// const initialState = {
+//   counter: 0,
+//   text: "",
+//   list: [],
+// };
 
-const INCREASE = "INCREASE";
-const DECREASE = "DECREASE";
-const CHANGE_TEXT = "CHANGE_TEXT";
-const ADD_TO_LIST = "ADD_TO_LIST";
+// const INCREASE = "INCREASE";
+// const DECREASE = "DECREASE";
+// const CHANGE_TEXT = "CHANGE_TEXT";
+// const ADD_TO_LIST = "ADD_TO_LIST";
 
-const increase = () => ({
-  type: "INCREASE",
-});
+// const increase = () => ({
+//   type: "INCREASE",
+// });
 
-const decrease = () => ({
-  type: "DECREASE",
-});
+// const decrease = () => ({
+//   type: "DECREASE",
+// });
 
-const changeText = (text) => ({
-  type: CHANGE_TEXT,
-  text,
-});
+// const changeText = (text) => ({
+//   type: CHANGE_TEXT,
+//   text,
+// });
 
-const addToList = (item) => ({
-  type: ADD_TO_LIST,
-  item,
-});
+// const addToList = (item) => ({
+//   type: ADD_TO_LIST,
+//   item,
+// });
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
-    case DECREASE:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
-    case CHANGE_TEXT:
-      return {
-        ...state,
-        text: action.text,
-      };
-    case ADD_TO_LIST:
-      return {
-        ...state,
-        list: state.list.concat(action.item),
-      };
-    default:
-      return state;
-  }
-}
+// function reducer(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         ...state,
+//         counter: state.counter + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         ...state,
+//         counter: state.counter - 1,
+//       };
+//     case CHANGE_TEXT:
+//       return {
+//         ...state,
+//         text: action.text,
+//       };
+//     case ADD_TO_LIST:
+//       return {
+//         ...state,
+//         list: state.list.concat(action.item),
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
-const store = createStore(reducer);
-console.log(store.getState());
+// const store = createStore(reducer);
+// console.log(store.getState());
 
-const listener = () => {
-  const state = store.getState();
-};
+// const listener = () => {
+//   const state = store.getState();
+// };
 
-const unsubscribe = store.subscribe(listener);
+// const unsubscribe = store.subscribe(listener);
 
-window.store = store;
+// store.dispatch(increase());
+// store.dispatch(decrease());
+// store.dispatch(changeText("안녕하세요"));
+// store.dispatch(addToList({ id: 1, text: "ㅎㅎ" }));
+
+// window.store = store;
